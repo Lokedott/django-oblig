3cf489dc8f299eabddab2ed976b313aade177f42
Lars Torgersen <larstorgersen100@hotmail.com>
2015-03-05 08:53:07 +0100

    Lagt til at man kan modifisere i admin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,11 +1,4 @@
 from django.contrib import admin
 from polls.models import Question
-# Register your models here.
-
-
 
 admin.site.register(Question)
-
-models.DecimalField(..., max_digits=5, decimal_places=2)
-
-models.DecimalField(..., max_digits=19, decimal_places=10)
