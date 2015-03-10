df086b1106b436b2768495ac4bbfed58cf79ca00
Lars Torgersen <larstorgersen100@hotmail.com>
2015-03-10 11:40:12 +0100

    Har lagt til en url import view i polls

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf.urls import patterns, url
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+
+
+from polls import views
+
+urlpatterns = patterns('',
+    url(r'^$', views.index, name='index'),
+)
