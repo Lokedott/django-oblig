144dd7d0909a1739100ad011948d9fd256b4f89a
Lars Torgersen <larstorgersen100@hotmail.com>
2015-03-12 11:26:59 +0100

    har lagt til en gjengi kommando for å slippe  loader , RequestContext hver gang

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from polls.models import Question
+from django.http import Http404
+#from django.template import RequestContext, loader
 
 
 def index(request):
@@ -16,10 +18,15 @@ def results(request, question_id):
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
 
-    from django.http import HttpResponse
-
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([p.question_text for p in latest_question_list])
-    return HttpResponse(output)
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+    def detail(request, question_id):
+    	try:
+    		question = Question.objects.get(pk=question_id)
+    	except Question.DoesNotExist:
+    		raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
