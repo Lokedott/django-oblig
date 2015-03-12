f814cf892208c5e93c4cc9fd4ae1e7f6596456fa
Lars Torgersen <larstorgersen100@hotmail.com>
2015-03-12 12:25:14 +0100

    har fjernet hardkode i url. Da man kan lettere fikse enkelte urler

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,22 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from django.conf.urls import patterns, url
-
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
-
-
-
 from polls import views
 
-urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-)
-
-
-urlpatterns = patterns('',
+urlpatterns = patterns('', 
     # ex: /polls/
     url(r'^$', views.index, name='index'),
     # ex: /polls/5/
